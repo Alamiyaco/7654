bdc9845828c796305a8f9c9290393ab1ebce62af
Muntadher Al-Hafidh <105360197+Alamiyaco@users.noreply.github.com>
2026-04-18 11:12:58 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+window.APP_CONFIG = {
+  googleSheetCsvUrl: "https://api.allorigins.win/raw?url=https://docs.google.com/spreadsheets/d/1hOmDl89od4_3mcFCGgQFwMbTF50SfbJinPNU1H17d4o/export?format=csv&gid=0"
+};
